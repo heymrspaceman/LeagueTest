26ee4db5b8598ba8791a427927c59032b7cccbbe
heymrspaceman <tam.mackenzie@gmail.com>
2012-07-22 20:40:39 +0100

    Code added - first import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeaguesTest::Application.config.secret_token = 'cc0cc2b355d63b3ec0063b016a367f1133f6deb35953dc759fd60169c1fd4329a19b6f46ffa46786ee3f8b384b4f36d365b5f716ed535bd8ecc5a4737e4be502'
